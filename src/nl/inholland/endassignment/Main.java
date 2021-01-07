b6ab6ddedb3835ca143135d9734fd895bc15fcf2
ohururo <robertohuru@gmail.com>
2021-01-07 19:56:06 +0100

    hello

--- src/nl/inholland/endassignment/Main.java
+++ src/nl/inholland/endassignment/Main.java
@@ -3,8 +3,9 @@ package nl.inholland.endassignment;
 import javafx.application.Application;
 
 public class Main {
-
         public static void main(String[] args) {
-        Application.launch(MainApplication.class, args);
+            System.out.println("dsfsdf");
+
+            Application.launch(MainApplication.class, args);
     }
 }
